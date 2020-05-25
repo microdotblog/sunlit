177d6b2e7d67b2b33dd3f1c5b3ece9a1126d8d67
Jonathan Hays <jon@silverpinesoftware.com>
2020-05-24 23:58:28 -0700

    Implemented drag and drop interface

--- Sunlit/ComposeViewController.swift
+++ Sunlit/ComposeViewController.swift
@@ -14,26 +14,18 @@ class SunlitStorySection {
 }
 
 
-class ComposeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDropDelegate, UICollectionViewDelegateFlowLayout {
+class ComposeViewController: UIViewController {
 
 	@IBOutlet var collectionView : UICollectionView!
 	var sections : [SunlitStorySection] = []
 	var needsInitialFirstResponder = true
+	var sectionToAddImage = 0
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-		if let flowLayout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-			flowLayout.estimatedItemSize = CGSize(width: self.view.bounds.size.width / 3.0, height:  self.view.bounds.size.width / 3.0)
-			flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-		}
-		self.collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-		
-		self.navigationItem.title = "New Post"
-		let rightItems : [UIBarButtonItem] = [UIBarButtonItem(title: "Post", style: .plain, target: self, action: #selector(onPost)) ,
-											  /*UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(onAddPhoto))*/ ]
-		self.navigationItem.rightBarButtonItems = rightItems
-		self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(onCancel))
+		self.configureCollectionView()
+		self.configureNavigationController()
     }
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -41,12 +33,31 @@ class ComposeViewController: UIViewController, UICollectionViewDelegate, UIColle
 		
 		self.navigationController?.setNavigationBarHidden(false, animated: true)
 	}
+	
+	func configureCollectionView() {
+		if let flowLayout = self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+			flowLayout.estimatedItemSize = CGSize(width: self.view.bounds.size.width / 3.0, height:  self.view.bounds.size.width / 3.0)
+			flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+		}
+		self.collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+		self.collectionView.dragInteractionEnabled = true
+	}
+	
+	func configureNavigationController() {
+		self.navigationItem.title = "New Post"
+		let rightItems : [UIBarButtonItem] = [UIBarButtonItem(title: "Post", style: .plain, target: self, action: #selector(onPost)) ,
+											  /*UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(onAddPhoto))*/ ]
+		self.navigationItem.rightBarButtonItems = rightItems
+		self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(onCancel))
+	}
     
 	/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	MARK: -
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
 
-	@objc func onAddPhoto() {
+	@objc func onAddPhoto(_ section : Int) {
+		self.sectionToAddImage = section
+		
 		let pickerController = UIImagePickerController()
 		pickerController.delegate = self
 		pickerController.allowsEditing = true
@@ -63,18 +74,15 @@ class ComposeViewController: UIViewController, UICollectionViewDelegate, UIColle
 		self.navigationController?.dismiss(animated: true, completion: nil)
 	}
 	
-	/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	MARK: -
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
-
 	func addImage(_ image : UIImage) {
-		if self.sections.count == 0 {
+		if self.sectionToAddImage >= self.sections.count {
 			let section = SunlitStorySection()
 			section.text = ""
 			section.images.append(image)
 			self.sections.append(section)
 		}
-		else if let section = self.sections.last {
+		else {
+			let section = self.sections[self.sectionToAddImage]
 			section.images.append(image)
 		}
 
@@ -82,22 +90,39 @@ class ComposeViewController: UIViewController, UICollectionViewDelegate, UIColle
 			self.collectionView.reloadData()
 		}
 	}
+}
 
-	
-	/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	MARK: -
-	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MARK: -
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+
+extension ComposeViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
 	func numberOfSections(in collectionView: UICollectionView) -> Int {
-		return self.sections.count
+		return self.sections.count + 1
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		
+		// Special case for the "Add new section" button cell...
+		if section >= self.sections.count {
+			return 1
+		}
+		
 		return self.sections[section].images.count + 2
 	}
 
 	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
+		
+		// Special case for the "Add new section" button cell...
+		if indexPath.section >= self.sections.count {
+			let size = PostImageCollectionViewCell.size(collectionView.bounds.size.width)
+			return size
+		}
+		
 		let section = self.sections[indexPath.section]
 
 		if indexPath.item == 0 {
@@ -115,6 +140,12 @@ class ComposeViewController: UIViewController, UICollectionViewDelegate, UIColle
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		
+		// Special case for the "Add new section" button cell...
+		if indexPath.section >= self.sections.count {
+			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostAddPhotoCollectionViewCell", for: indexPath) as! PostAddPhotoCollectionViewCell
+			return cell
+		}
 		
 		let sectionData = self.sections[indexPath.section]
 		
@@ -146,19 +177,169 @@ class ComposeViewController: UIViewController, UICollectionViewDelegate, UIColle
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-		let sectionData = self.sections[indexPath.section]
-		if indexPath.item > sectionData.images.count {
-			self.onAddPhoto()
+		
+		// Special case for the "Add new section" button cell...
+		if indexPath.section >= self.sections.count {
+			self.onAddPhoto(indexPath.section)
+		}
+		else {
+			let sectionData = self.sections[indexPath.section]
+			if indexPath.item > sectionData.images.count {
+				self.onAddPhoto(indexPath.section)
+			}
 		}
 		
 		collectionView.deselectItem(at: indexPath, animated: true)
 	}
-	
-	func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+}
+
+
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MARK: -
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+extension ComposeViewController : UICollectionViewDropDelegate, UICollectionViewDragDelegate {
+
+	func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+		let section = self.sections[indexPath.section]
+		let image = section.images[indexPath.item - 1]
+		let itemProvider = NSItemProvider(object: image)
+		let dragItem = UIDragItem(itemProvider: itemProvider)
 		
+		return [dragItem]
+	}
+
+
+	func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+		
+		if let destination = destinationIndexPath {
+			
+			// Check to see if it's being dragged to an uncreated section (at the bottom)
+			if destination.section >= self.sections.count {
+				let proposal = UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+				return proposal
+			}
+			
+			// Check to see if it's being dragged to the title section or to the "add photo" button and deny it...
+			let section = self.sections[destination.section]
+			if destination.item > section.images.count || destination.item == 0 {
+				let proposal = UICollectionViewDropProposal(operation: .forbidden)
+				return proposal
+			}
+			
+			// Otherwise, we are good to move it...
+			let proposal = UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+			return proposal
+		}
+		else {
+			// For now, we aren't going to support deleting from here...
+			let proposal = UICollectionViewDropProposal(operation: .move, intent: .insertIntoDestinationIndexPath)
+			return proposal
+		}
+	}
+
+	func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+		if let destinationIndexPath = coordinator.destinationIndexPath,
+		   let drop = coordinator.items.first,
+		   let sourceIndexPath = drop.sourceIndexPath{
+			
+			let sourceSection = self.sections[sourceIndexPath.section]
+			let image = sourceSection.images[sourceIndexPath.item - 1]
+			sourceSection.images.remove(at: sourceIndexPath.item - 1)
+			
+			let deleteSection = sourceSection.images.count == 0
+			var insertSection = false
+			
+			if destinationIndexPath.section < self.sections.count {
+				let destSection = self.sections[destinationIndexPath.section]
+				destSection.images.insert(image, at: destinationIndexPath.item - 1)
+			}
+			else {
+				let section = SunlitStorySection()
+				section.text = ""
+				section.images.append(image)
+				self.sections.append(section)
+				insertSection = true
+			}
+			
+			let insertItems = [destinationIndexPath]
+			let deleteItems = [sourceIndexPath]
+			
+			if deleteSection {
+				self.sections.remove(at: sourceIndexPath.section)
+			}
+			
+			self.collectionView.performBatchUpdates({
+				
+				if deleteSection {
+					let destIndexPath = IndexPath(item: destinationIndexPath.item, section: destinationIndexPath.section - 1)
+					self.collectionView.deleteSections(NSIndexSet(index: sourceIndexPath.section) as IndexSet)
+					if insertSection {
+						self.collectionView.insertSections(NSIndexSet(index: destIndexPath.section) as IndexSet)
+					}
+					
+					self.collectionView.insertItems(at: [destIndexPath])
+				}
+				else {
+					self.collectionView.deleteItems(at: deleteItems)
+					self.collectionView.insertItems(at: insertItems)
+				}
+			})
+			{ (complete) in
+			}
+			
+		}
 	}
 	
+	
+	/*
+	
+	- (void) collectionView:(UICollectionView *)collectionView performDropWithCoordinator:(id<UICollectionViewDropCoordinator>)coordinator
+	{
+		NSIndexPath* destinationIndexPath = coordinator.destinationIndexPath;
+	  
+		id<UICollectionViewDropItem> drop = [coordinator.items firstObject];
+		UIDragItem* item = drop.dragItem;
+		
+		SLEntry* dragged_e = item.localObject;
+		
+		NSIndexSet* deleting_section = [self.story removeEntry:dragged_e];
+		if (deleting_section)
+		{
+			NSUInteger removedSection = deleting_section.firstIndex;
+			
+			if (removedSection < destinationIndexPath.section)
+			{
+				destinationIndexPath = [NSIndexPath indexPathForItem:destinationIndexPath.item inSection:destinationIndexPath.section - 1];
+			}
+		}
+		
+		[self.story move:dragged_e to:destinationIndexPath];
+		
+		[self.entriesCollectionView performBatchUpdates:^{
+			if (deleting_section) {
+				[self.entriesCollectionView deleteSections:deleting_section];
+			}
+			else {
+				[self.entriesCollectionView deleteItemsAtIndexPaths:@[ drop.sourceIndexPath ]];
+			}
+			[self.entriesCollectionView insertItemsAtIndexPaths:@[ destinationIndexPath ]];
+		}
+		completion:^(BOOL finished)
+		{
+		}];
+
+	}
+
+
+	*/
 }
+
+
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MARK: -
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
 
 extension ComposeViewController : UITextViewDelegate {
 	
@@ -175,6 +356,11 @@ extension ComposeViewController : UITextViewDelegate {
 		return true
 	}
 }
+
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MARK: -
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
 
 extension ComposeViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 	
@@ -196,6 +382,11 @@ extension ComposeViewController : UIImagePickerControllerDelegate, UINavigationC
 	
 }
 
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MARK: -
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+
 class PostImageCollectionViewCell : UICollectionViewCell {
 	@IBOutlet var postImage : UIImageView!
 	@IBOutlet var widthConstraint : NSLayoutConstraint!
@@ -205,6 +396,10 @@ class PostImageCollectionViewCell : UICollectionViewCell {
 		return CGSize(width: size, height: size)
 	}
 }
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MARK: -
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
 
 class PostTextCollectionViewCell : UICollectionViewCell {
 	@IBOutlet var postText : UITextView!
@@ -223,6 +418,11 @@ class PostTextCollectionViewCell : UICollectionViewCell {
 	}
 
 }
+
+
+/* ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MARK: -
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
 
 class PostAddPhotoCollectionViewCell : UICollectionViewCell {
 }
