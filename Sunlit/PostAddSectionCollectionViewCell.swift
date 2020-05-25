3b27344db653678c27293a93139b05137a6a6888
Manton Reece <manton@me.com>
2020-05-25 13:22:52 -0500

    Added new collection cell for New Section, tweaked spacing and corners.

---
+++ Sunlit/PostAddSectionCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostAddSectionCollectionViewCell.swift
+//  Sunlit
+//
+//  Created by Manton Reece on 5/25/20.
+//  Copyright © 2020 Micro.blog, LLC. All rights reserved.
+//
+
+import UIKit
+
+class PostAddSectionCollectionViewCell: UICollectionViewCell {
+
+	static func size(_ collectionViewWidth : CGFloat) -> CGSize {
+		return CGSize(width: collectionViewWidth, height: 60)
+	}
+	
+}
