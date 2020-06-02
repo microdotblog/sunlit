6b6a1fdb5e5516c9717ec5a73074fbb9d837f149
Jonathan Hays <jon@silverpinesoftware.com>
2020-06-01 18:28:46 -0700

    Adding support for basic image viewer

--- Sunlit/ImageViewerViewController.swift
+++ Sunlit/ImageViewerViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
-class ImageViewerViewController: UIViewController {
+class ImageViewerViewController: UIViewController, UIScrollViewDelegate {
 
 	@IBOutlet var image : UIImageView!
+	@IBOutlet var scrollView : UIScrollView!
+	
 	var pathToImage = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
 		self.navigationItem.leftBarButtonItem = UIBarButtonItem.barButtonWithImage(named: "back_button", target: self, action: #selector(dismissViewController))
-    
+		self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(onShare))
+		self.navigationItem.rightBarButtonItem?.tintColor = .black
+		
 		if let image = ImageCache.prefetch(pathToImage) {
 			self.image.image = image
 		}
@@ -28,10 +32,31 @@ class ImageViewerViewController: UIViewController {
 					}
 			}
 		}
+		
+		self.scrollView.contentSize = self.image.frame.size
+		self.image.frame = self.scrollView.bounds
 	}
 
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		self.image.frame = self.scrollView.bounds
+	}
+	
+	@objc func onShare() {
+		
+		if let image = ImageCache.prefetch(self.pathToImage) {
+			let items : [Any] = [image]
+			let activities : [UIActivity]? = nil
+			let activityViewController = UIActivityViewController(activityItems: items, applicationActivities: activities)
+			self.present(activityViewController, animated: true, completion: nil)
+		}
+	}
 	
 	@objc func dismissViewController() {
 		self.navigationController?.popViewController(animated: true)
+	}
+	
+	func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+		return self.image
 	}
 }
