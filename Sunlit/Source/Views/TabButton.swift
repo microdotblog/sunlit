e6d0d1bdd4582bdc9be5235eb465a870d7b9bd9c
Manton Reece <manton@me.com>
2020-07-21 22:30:53 -0500

    Fixed tab icons in Dark Mode, updating selected color.

--- Sunlit/Source/Views/TabButton.swift
+++ Sunlit/Source/Views/TabButton.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class TabButton: UIButton {
 
+	var defaultImage: UIImage?
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		
+		self.defaultImage = self.image(for: .normal)
+	}
+	
     override var isSelected: Bool {
         didSet {
+			if self.isSelected {
+				self.setTitleColor(UIColor(named: "color_tab_selected"), for: .normal)
+				self.setTitleColor(UIColor(named: "color_tab_selected"), for: .selected)
+
+//				self.setImage(self.defaultImage, for: .normal)
+//				self.setImage(self.defaultImage, for: .selected)
+			}
+			else {
+				self.setTitleColor(UIColor(named: "color_tab_normal"), for: .normal)
+				self.setTitleColor(UIColor(named: "color_tab_normal"), for: .selected)
+
+//				self.setImage(self.defaultImage, for: .normal)
+//				self.setImage(self.defaultImage, for: .selected)
+			}
         }
     }
 
+	override func draw(_ rect: CGRect) {
+		if self.isSelected {
+//			let c = UIColor.white
+//			c.set()
+//
+//			let r = self.contentRect(forBounds: self.bounds)
+//
+//			let path = UIBezierPath(roundedRect: r, cornerRadius: 10)
+//			path.fill()
+		}
+		
+		super.draw(rect)
+	}
+	
 }
