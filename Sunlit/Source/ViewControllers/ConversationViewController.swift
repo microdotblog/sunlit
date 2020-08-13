34f43c6642856fcf4eec8849080e573a5aae2c06
Jonathan Hays <jon@silverpinesoftware.com>
2020-08-13 10:23:43 -0700

    Moving conversation post object construction to the main thread.

--- Sunlit/Source/ViewControllers/ConversationViewController.swift
+++ Sunlit/Source/ViewControllers/ConversationViewController.swift
@@ -82,14 +82,15 @@ class ConversationViewController: UIViewController {
 	@objc func loadConversation() {
 		if let post = sourcePost {
 			Snippets.shared.fetchConversation(post: post) { (error, posts : [SnippetsPost]) in
-				self.posts = [ ]
-				
-				for post in posts {
-					let sunlitPost = SunlitPost.create(post)
-					self.posts.insert(sunlitPost, at: 0)
-				}
 				
 				DispatchQueue.main.async {
+					self.posts = [ ]
+					
+					for post in posts {
+						let sunlitPost = SunlitPost.create(post)
+						self.posts.insert(sunlitPost, at: 0)
+					}
+
 					self.tableView.reloadData()
 					self.spinner.isHidden = true
 					self.tableViewRefreshControl.endRefreshing()
